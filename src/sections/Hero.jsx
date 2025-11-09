9bc53d726981017ea59dc17f3237aad6833dda27
abdulrafayb <abdulrafay200066@gmail.com>
2025-11-09 23:57:33 +0500

    added navbar and working on hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,16 @@
+function Hero() {
+  return (
+    <div>
+      <div className='main-container h-screen flex flex-col lg:justify-center items-start lg:py-12 max-lg:pt-40'>
+        <h1 className='text-3xl lg:text-[3.2vw] uppercase font-heading font-semibold'>
+          Abdul Rafay
+        </h1>
+        <h2 className='text-6xl lg:text-[8vw] font-heading font-bold leading-none tracking-tight mt-3 mb-6'>
+          Web Developer <br /> & <span>Designer</span>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
