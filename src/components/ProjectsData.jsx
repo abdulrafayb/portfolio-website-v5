0ec5b30a36f9b6de5ae15f81e19ac074f94daed9
abdulrafayb <abdulrafay200066@gmail.com>
2025-11-24 01:20:13 +0500

    made progress in work and marquee sections

---
+++ src/components/ProjectsData.jsx
@@ -0,0 +1,26 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: 'Project One',
+    image: 'Description for project one.',
+    link: '',
+  },
+  {
+    id: 2,
+    name: 'Project Two',
+    image: 'Description for project two.',
+    link: '',
+  },
+  {
+    id: 3,
+    name: 'Project Three',
+    image: 'Description for project three.',
+    link: '',
+  },
+  {
+    id: 4,
+    name: 'Project Three',
+    image: 'Description for project three.',
+    link: '',
+  },
+];
--- src/sections/Marquee.jsx
+++ src/sections/Marquee.jsx
@@ -1,5 +1,29 @@
+import star from '/public/star.svg';
+
 function Marquee() {
-  return <div></div>;
+  return (
+    <div className='bg-white text-black pb-28 lg:pb-44 overflow-hidden'>
+      <div className='whitespace-nowrap animate-marquee text-6xl lg:text-[7vw] font-heading font-semibold leading-none tracking-tight'>
+        <span className='flex gap-4 lg:gap-8 mx-8'>
+          CREATE <img src={star} alt='' className='marquee-star-one w-[7vw]' />
+          DESIGN <img src={star} alt='' className='marquee-star-one w-[7vw]' />
+          INSPIRE <img src={star} alt='' className='marquee-star-one w-[7vw]' />
+          DESIGN <img src={star} alt='' className='marquee-star-one w-[7vw]' />
+          INSPIRE <img src={star} alt='' className='marquee-star-one w-[7vw]' />
+        </span>
+      </div>
+
+      <div className='whitespace-nowrap animate-marquee-reverse text-6xl lg:text-[7vw] font-heading font-semibold leading-none tracking-tight'>
+        <span className='flex gap-4 lg:gap-8 mx-8'>
+          CREATE <img src={star} alt='' className='marquee-star-two w-[7vw]' />
+          DESIGN <img src={star} alt='' className='marquee-star-two w-[7vw]' />
+          INSPIRE <img src={star} alt='' className='marquee-star-two w-[7vw]' />
+          DESIGN <img src={star} alt='' className='marquee-star-two w-[7vw]' />
+          INSPIRE <img src={star} alt='' className='marquee-star-two w-[7vw]' />
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Marquee;
